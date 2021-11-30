3dffdefea4069a0d1bf2b265957f640c3efecde2
Kate Brownbill <glowkate@gmail.com>
2021-11-30 11:55:53 -0500

    Updated staybot_led_strip example to include semi-helpful comments.

--- 2021/staybot_led_strip/src/main/java/frc/robot/Robot.java
+++ 2021/staybot_led_strip/src/main/java/frc/robot/Robot.java
@@ -8,11 +8,10 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This is a demo program showing the use of the RobotDrive class, specifically it contains the code
- * necessary to operate a robot with tank drive.
+ * This is a demo program showing off the strip of LED lights and how you can
+ * control and change what each light along the strip displayed.
  */
 public class Robot extends TimedRobot {
   private Joystick m_joystick;
@@ -22,30 +21,50 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_joystick = new Joystick(0);
+    //The input for the AddressableLED class is based on its port
     m_led = new AddressableLED(3);
+    //The input for the AddressableLEDBuffer is the length of the LED strip. We have 8 lights on ours, so we input 8.
     m_ledBuffer = new AddressableLEDBuffer(8);
+
+    //This sets the initial colour of each of the LEDs. They are all set to red here.
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      m_ledBuffer.setRGB(i, 255, 255, 255);
+      m_ledBuffer.setRGB(i, 225, 0, 0);
     }
     
+    //This sets the length of the AddressableLED to the same value that we set the buffer to.
     m_led.setLength(m_ledBuffer.getLength());
 
+    //setData updates the value of the LED strip to whatever you put in the buffer.
     m_led.setData(m_ledBuffer);
+    //start causes the LEDs to be updated every second.
     m_led.start();
   }
 
   @Override
   public void teleopPeriodic() {
+    //Grab the position of the joystick
     double joystickX = m_joystick.getX();
     double joystickY = m_joystick.getY();
 
+    //The x value of the joystick corisponds to what LED we change the colour of. This sets the value from 1-8.
     int targetLED = (int)(7 * (0.5 + (joystickX / 2)));
+    //The y value of the joystick corisponds to what colour the LED is changed to. This sets the value from -127.5 - 127.5.
     int LEDcolour = (int)(255 * (joystickY) / 2);
 
+    //Sets each of the colours. Note that the redLEDcolour 
     int redLEDcolour = Math.abs(LEDcolour) * 2;
     int greenLEDcolour = (int)127.5 + LEDcolour;
     int blueLEDcolour = (int)127.5 - LEDcolour;
     
+  /**
+    * Sets each of the lights on the strip to a diffrent colour.
+    * A light designated by the x value of the joystick is the target light.
+    * Lights nearby the target light also change colour.
+    * The if statements detect this and adjust the light accordingly.
+    * The default position for the lights when not modified is red.
+    * setRGB takes the LED on the strip to change, 
+    * followed by the RGB value you want to set the light to.
+    */ 
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       if(i == targetLED){
         m_ledBuffer.setRGB(i, 255 - redLEDcolour, greenLEDcolour, blueLEDcolour);
@@ -60,6 +79,7 @@ public class Robot extends TimedRobot {
         m_ledBuffer.setRGB(i, 255, 0, 0);
       }
     }
+    //setData again to update the strip.
     m_led.setData(m_ledBuffer);
   }
 }
